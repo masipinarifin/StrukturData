1ec39e87981f984c10bf4191f15b1b2956eb698d
masipinarifin <masipinarifin@gmail.com>
2021-05-04 16:42:52 +0700

    Uji Coba Teori 9

---
+++ src/Teori9/Stack.java
@@ -0,0 +1,15 @@
+
+package Teori9;
+
+/**
+ *
+ * @Nurul Arifin 19MI0016
+ */
+public class Stack {
+    private Stack(){
+         
+    }
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
